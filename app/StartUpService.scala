078afc8589ed4070f22ab76df3ab8e561bd32a05
khans <sami.cseseu@gmail.com>
2018-08-14 02:36:40 +0600

    Scala post request method add and demo save ip into instance registry

--- app/StartUpService.scala
+++ app/StartUpService.scala
@@ -10,14 +10,35 @@ import scala.util.{Failure, Success}
   */
 @Singleton
 class StartUpService{
-  val url = ConfigFactory.load().getString("instance-registry-path")
-  getRequest(url)
 
-  //get request with a url
-  def getRequest(url: String) = {
+  checkInstanceRegistry()
+  storeIpToInstanceRegistry()
+
+  //check instance registry exist
+  def checkInstanceRegistry() = {
+    val url = ConfigFactory.load().getString("instance-registry-path")
     val result = BlockingHttpClient.doGet(url)
     result match {
       case Success(response) => println(response)
+      case Failure(_) => println("Failed to connect with instance registry")
+    }
+  }
+
+  //store ip to registry db
+  def storeIpToInstanceRegistry(): Unit =
+  {
+    val instanceRegistryUrl = ConfigFactory.load().getString("instance-registry-path")
+    val result = BlockingHttpClient.doGet(instanceRegistryUrl)
+    result match {
+      case Success(response) => {
+        val postData = s"""{"ip":"$instanceRegistryUrl"}"""
+        val urlIpStore = "http://0.0.0.1"
+        val instanceSaveRes = BlockingHttpClient.doPost(urlIpStore, postData)
+        result match {
+          case Success(response) => println("Instance save successfully")
+          case Failure(_) => println(instanceSaveRes)
+        }
+      }
       case Failure(_) => println("Failed to connect with instance registry")
     }
   }
--- app/utils/BlockingHttpClient.scala
+++ app/utils/BlockingHttpClient.scala
@@ -3,7 +3,7 @@ package utils
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse, Uri}
+import akka.http.scaladsl.model.{HttpEntity, MediaTypes, HttpMethods, HttpRequest, HttpResponse, Uri}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import akka.util.ByteString
 
@@ -38,10 +38,43 @@ object BlockingHttpClient {
 
   }
 
+  // data parameter will be """{"name":"Hello"}"""
+  def doPost(uri: Uri, data: String) = {
+    implicit val system = ActorSystem()
+    implicit val executionContext = system.dispatcher
+    implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system))
+    val bdata = ByteString(data)
+    try {
+      val req: Future[HttpResponse] = Http(system).singleRequest(HttpRequest(
+        method = HttpMethods.POST,
+        uri = uri,
+        entity = bdata
+      ))
+      Await.result(req, Duration.Inf)
+
+      val f = req.value.get.get.entity.dataBytes.runFold(ByteString(""))(_ ++ _)
+      Await.result(f, Duration.Inf)
+
+      Success(f.value.get.get.utf8String)
+    } catch  {
+      case e : Exception => Failure(e)
+    } finally {
+      system.terminate()
+      Await.result(system.whenTerminated, Duration.Inf)
+    }
+  }
+
   def executeGet(target: String, server: String) : Try[String] = {
 
     val uri = Uri(server)
     doGet(uri.withPath(uri.path + target))
+
+  }
+
+  def executePost(target: String, server: String, data: String) : Try[String] = {
+
+    val uri = Uri(server)
+    doPost(uri.withPath(uri.path + target), data)
 
   }
 
