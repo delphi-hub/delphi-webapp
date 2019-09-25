aab681d54aacf21072d17e5398498f2eec4f484a
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2019-09-25 21:51:04 +0200

    Landing page functionalities for metrics
    Metric list and filterring

--- frontend/client/src/MenuStepOne.vue
+++ frontend/client/src/MenuStepOne.vue
@@ -3,16 +3,12 @@
         <div class="card">					
             <div class="card-body">
                 <h6 class="card-title">Step 1</h6>
-                <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown">Choose Metric</button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#" data-toggle="popover" data-trigger="hover" title="Information about Metric 1" data-placement="right" data-content="Information Information Information">Metric 1</a>
-                    <a class="dropdown-item" href="#" data-toggle="popover" data-trigger="hover" title="Information about Metric 2" data-placement="right" data-content="Information Information Information">Metric 2</a>
-                    <a class="dropdown-item" href="#" data-toggle="popover" data-trigger="hover" title="Information about Metric 3" data-placement="right" data-content="Information Information Information">Metric 3</a>
-                    <a class="dropdown-item" href="#" data-toggle="popover" data-trigger="hover" title="Information about Metric 4" data-placement="right" data-content="Information Information Information">Metric 4</a>
-                    <a class="dropdown-item" href="#" data-toggle="popover" data-trigger="hover" title="Information about Metric 5" data-placement="right" data-content="Information Information Information">Metric 5</a>
-                    <a class="dropdown-item" href="#" data-toggle="popover" data-trigger="hover" title="Information about Metric 6" data-placement="right" data-content="Information Information Information">Metric 6</a>
-                </div>
-            </div>
+                <input type="text" id="filter" name="metric_suggest" onkeyup="filter1()" size="15">
+										<div>	
+											<select id="select" size="10" style="width: 165px;height: 120px;">
+											</select>
+										</div>
+             </div>
         </div>
     </div>
 </template>
@@ -20,7 +16,6 @@
 <script>
 
 </script>
-
 
 <style>
     #steponeCol {
--- frontend/client/src/main.js
+++ frontend/client/src/main.js
@@ -4,11 +4,10 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchPart from './SearchPart.vue';
 import ResultPart from './ResultPart.vue';
-
 Vue.component('search-part', SearchPart);
 Vue.component('result-part', ResultPart);
-
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App)  
 })
+
