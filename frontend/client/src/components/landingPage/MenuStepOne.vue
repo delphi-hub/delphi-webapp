fe72a9ab4a6144097599123330d93f030277d056
SViktorS <viktor.schimpf90@web.de>
2019-11-27 18:36:32 +0100

    Revert "Merge branch 'feature/landingPage' of https://github.com/delphi-hub/delphi-webapp into feature/landingPage"
    This reverts commit d3d6e18fb996e3fff951fbbefc979e0d2c70d059, reversing
    changes made to 5da9fa738246998a359f02b47e16649f1fd65162.

--- frontend/client/src/components/landingPage/MenuStepOne.vue
+++ frontend/client/src/components/landingPage/MenuStepOne.vue
@@ -11,10 +11,10 @@
                     </div>
                 </div>    
                 <div v-show="!selectedMetric">
-                    <input type="text" id="filter" name="metric_suggest" v-on:keyup="filter1" size="15" :value="selectedMetric"></input> 
+                    <input type="text" id="filter" name="metric_suggest" v-on:keyup="filter1" size="15" :value="selectedMetric"> 
 		        	<div>	 
                         <select id="select" size="10" v-model="selectedMetric" @change="sendMetric">
-                            <option id="optionSelect" v-for="data in info.data" v-bind:key="data">{{data}}</option>
+                            <option v-for="data in info.data"  v-bind:key="data">{{data}}</option>
                         </select>      
 			    	</div>
                 </div>                
@@ -68,8 +68,6 @@ export default {
         removeMetric(){
             this.selectedMetric = null;
             this.$emit('metricSent', this.selectedMetric);
-            document.getElementById("filter").value = ''; //without these, after removing metric the list would still be filtered
-            this.filter1();                                 //even though the text field is empty
         },
         filter1() {
             var keyword = document.getElementById("filter").value;
@@ -77,20 +75,19 @@ export default {
             for (var i = 0; i < select.length; i++) {
                 var txt = select.options[i].text;
 		        var txt2 = keyword.toLowerCase();
-                if (txt.toLowerCase().includes(txt2)) {
-			        select.options[i].style.display = 'list-item';           
-                }
-                else {
-                    select.options[i].style.display = 'none';
-                }
+            if (txt.toLowerCase().includes(txt2)) {
+			    select.options[i].style.display = 'list-item';           
+            } else {
+                select.options[i].style.display = 'none';
+                     }
             }
         }
     },
-    mounted () {
-        axios
-        .get('https://delphi.cs.uni-paderborn.de/api/features')
-        .then(response => (this.info = response))
-    }
+  mounted () {
+    axios
+      .get('https://delphi.cs.uni-paderborn.de/api/features')
+      .then(response => (this.info = response))
+  }
 }
 </script>
 
@@ -122,16 +119,8 @@ export default {
         color:red;
         border-radius: 3px;
     }
-    #filter {
-        width: 250px;
-    }
     #select {
-        width: 250px;
+        width: 165px;
         height: 120px;
-        overflow: auto;
     }
-    #optionSelect:hover {
-        background-color:rgba(176, 240, 176, 0.753);
-    }
-    
 </style>
--- frontend/client/src/components/landingPage/QueryMenu.vue
+++ frontend/client/src/components/landingPage/QueryMenu.vue
@@ -1,30 +1,12 @@
 <template>
     <div class="col-6" id="menuCol">
         <div class="row">
-			<menuStepOne 
-				@metricSent="metric = $event" 
-				@confirmMetricReset="metricToReset = $event" 
-				:metricShouldBeReseted="metricToReset">
-			</menuStepOne>
-			<menuStepTwo 
-				@operatorSent="operator = $event" 
-				@valueSent="value = $event" 
-				@confirmOperatorAndValueReset="operatorAndValueToReset = $event" 
-				:operatorAndValueShouldBeReseted="operatorAndValueToReset">
-			</menuStepTwo>
-			<menuStepThree 
-				@logicalNotSent="logicalNOT = $event" 
-				@logicalOperatorSent="logicalOperator = $event" 
-				@confirmLNotAndLOperatorReset="lNotAndLOperatorToReset = $event" 
-				:lNOTAndLOperatorShouldBeReseted="lNotAndLOperatorToReset">
-			</menuStepThree>       
+            <menuStepOne @metricSent="metric = $event" @confirmMetricReset="metricToReset = $event" :metricShouldBeReseted="metricToReset"></menuStepOne>
+            <menuStepTwo @operatorSent="operator = $event" @valueSent="value = $event" @confirmOperatorAndValueReset="operatorAndValueToReset = $event" :operatorAndValueShouldBeReseted="operatorAndValueToReset"></menuStepTwo>
+            <menuStepThree @logicalNotSent="logicalNOT = $event" @logicalOperatorSent="logicalOperator = $event" @confirmLNotAndLOperatorReset="lNotAndLOperatorToReset = $event" :lNOTAndLOperatorShouldBeReseted="lNotAndLOperatorToReset"></menuStepThree>       
 		</div>
 		<hr>
-		<button class="btn btn-dark" id="addQueryButton" 
-			@click="onAddQuery" 
-			:style= "[(operator && metric && value && logicalOperator !== null) ? {'background-color': 'green'} : {'background-color':null}]">
-			<h5 id="addQueryButtonText">Add to Query</h5>
-		</button>		
+		<button class="btn btn-dark" id="addQueryButton" @click="onAddQuery" :style= "[(operator && metric && value && logicalOperator !== null) ? {'background-color': 'green'} : {'background-color':null}]"><h5 id="addQueryButtonText">Add to Query</h5></button>		
     </div>
 </template>
 
@@ -47,9 +29,11 @@
 				logicalNOT: null,
 				logicalOperator: null,
 				errors: [],
+				//queries: [],
 				metricToReset: false,
 				operatorAndValueToReset: false,
 				lNotAndLOperatorToReset: false
+			//TODO	isSearchable: false
             }
         },
 		methods: {
@@ -58,6 +42,14 @@
 					if(this.errors.length){
 						this.errors = [];
 					}
+					let queryCurrent = {
+						metric: this.metric,
+						operator: this.operator,
+						value: this.value,
+						logicalNOT: this.logicalNOT,
+						logicalOperator: this.logicalOperator
+					}
+					//this.queries.push(queryCurrent);
 
 					var out = '';		
 					if(this.logicalNOT) {																																					
@@ -69,6 +61,7 @@
 					if(this.logicalOperator){
 						out += ' ' + this.logicalOperator + ' ';
 					}
+					
 
 					this.$emit('addQuerySent', out);
 					this.metric = null;
@@ -88,7 +81,7 @@
 					}
 					else {
 							this.removeElement(this.errors, "Metric required.");
-					}
+						}
 					if(!this.operator) {
 						if(!this.errors.includes("Operator required.")){
 							this.errors.push("Operator required.");
@@ -96,7 +89,7 @@
 					}
 					else {
 							this.removeElement(this.errors, "Operator required.");
-					}
+						}
 					if(!this.value) {
 						if(!this.errors.includes("Value required.")){
 							this.errors.push("Value required.");
@@ -104,7 +97,7 @@
 					}
 					else {
 							this.removeElement(this.errors, "Value required.");	
-					}
+						}
 				}			
 			},
 			removeElement(array, elem) {
@@ -116,6 +109,7 @@
 		}
 	}
 </script>
+
 
 <style>
     #menuCol {
@@ -137,9 +131,9 @@
 	}
 
 	#addQueryButton:hover{
-    	background-color: rgb(97, 97, 97);
-    	box-shadow: 1px 1px 5px 3px grey;
-    	border-radius: 3px;
+    background-color: rgb(97, 97, 97);
+    box-shadow: 1px 1px 5px 3px grey;
+    border-radius: 3px;
 }
 
 #resetButton {
@@ -156,9 +150,9 @@
 	}
 
 	#resetButton:hover{
-    	background-color: rgb(97, 97, 97);
-    	box-shadow: 1px 1px 5px 3px grey;
-    	border-radius: 3px;
+    background-color: rgb(97, 97, 97);
+    box-shadow: 1px 1px 5px 3px grey;
+    border-radius: 3px;
 }
 
 </style>
