211687274a5e5e660172200c1026419834d977f9
chinmayk1613 <50278095+chinmayk1613@users.noreply.github.com>
2019-12-01 17:30:05 +0100

    Modified the HTTP request parameter

--- app/controllers/FrontController.scala
+++ app/controllers/FrontController.scala
@@ -15,25 +15,26 @@
 // limitations under the License.
 package controllers
 
+
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import javax.inject._
-
-import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.Configuration
-import play.api.http.HttpErrorHandler
 import play.api.mvc._
+import spray.json.DefaultJsonProtocol
 
-/**
-  * Frontend controller managing all static resource associate routes.
-  * @param assets Assets controller reference.
-  * @param cc Controller components reference.
-  */
 @Singleton
-class FrontController @Inject()(assets: Assets, errorHandler: HttpErrorHandler, config: Configuration, cc: ControllerComponents)
-  extends AbstractController(cc) with I18nSupport {
+class FrontController @Inject()(assets: Assets,configuration: Configuration, cc: ControllerComponents) extends AbstractController(cc)
+  with SprayJsonSupport with DefaultJsonProtocol {
+
 
   def startPoint: Action[AnyContent] = assets.at("index.html")
 
   def frontEndFile(resource: String): Action[AnyContent] =
     if (resource.contains(".")) assets.at(resource) else startPoint
 
+
 }
+
+
+
