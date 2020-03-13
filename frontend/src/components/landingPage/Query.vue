2b74e8e04543dd40fea99f99616f2aa837fe9be4
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2020-03-13 13:08:49 +0100

    Revert "Minor changes"
    This reverts commit 2f1d86803164389b2a7fd071ebb105846f7a041c.

--- frontend/src/components/landingPage/Query.vue
+++ frontend/src/components/landingPage/Query.vue
@@ -100,6 +100,10 @@
 </template>
 
 <script>
+/* (function(){function e(b,e,f){if(!h)throw Error("textarea-caret-position#getCaretCoordinates should only be called in a browser");if(f==f&&f.debug||!1){var a=document.querySelector("#input-textarea-caret-position-mirror-div");a&&a.parentNode.removeChild(a)}a=document.createElement("div");a.id="input-textarea-caret-position-mirror-div";document.body.appendChild(a);var c=a.style,d=window.getComputedStyle?window.getComputedStyle(b):b.currentStyle,k="INPUT"===b.nodeName;c.whiteSpace="pre-wrap";k||(c.wordWrap=
+"break-word");c.position="absolute";f||(c.visibility="hidden");l.forEach(function(a){k&&"lineHeight"===a?c.lineHeight=d.height:c[a]=d[a]});m?b.scrollHeight>parseInt(d.height)&&(c.overflowY="scroll"):c.overflow="hidden";a.textContent=b.value.substring(0,e);k&&(a.textContent=a.textContent.replace(/\s/g,"\u00a0"));var g=document.createElement("span");g.textContent=b.value.substring(e)||".";a.appendChild(g);b={top:g.offsetTop+parseInt(d.borderTopWidth),left:g.offsetLeft+parseInt(d.borderLeftWidth),height:parseInt(d.lineHeight)};
+f?g.style.backgroundColor="#aaa":document.body.removeChild(a);return b}var l="direction boxSizing width height overflowX overflowY borderTopWidth borderRightWidth borderBottomWidth borderLeftWidth borderStyle paddingTop paddingRight paddingBottom paddingLeft fontStyle fontVariant fontWeight fontStretch fontSize fontSizeAdjust lineHeight fontFamily textAlign textTransform textIndent textDecoration letterSpacing wordSpacing tabSize MozTabSize".split(" "),h="undefined"!==typeof window,m=h&&null!=window.mozInnerScreenX;
+"undefined"!=typeof module&&"undefined"!=typeof module.exports?module.exports=e:h&&(window.getCaretCoordinates=e)})(); */
 	import { required } from "vuelidate/lib/validators";
 	import { eventBus } from "../../main";
 
@@ -267,6 +271,7 @@
             let currentNewWord=this.currentInput.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, '\\$&');
             const regex = new RegExp("(" + currentNewWord + ")", "gi");
 
+            //word=escapeRegExp(word);
 			return word.replace(regex, '<mark>$1</mark>');
 				}
 				else
@@ -325,6 +330,7 @@
 			//if (this.currentInput !== "") {
 			if (typeof this.currentInput !== "undefined" && this.currentInput !== "" ) {
 				let metricDummy1=this.corpus;
+				//metricDummy1=metricDummy1.map(a => a.toUpperCase());
 				let i=0;
 				for (let index = 0; index < metricDummy1.length; index++) {
 					if(metricDummy1[index].toUpperCase().includes(this.currentInput.toUpperCase()))
