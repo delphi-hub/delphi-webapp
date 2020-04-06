103e1dc0db4282b70b0fc09f2b07af03bc41c9b0
Shashank Kanakapura Srivatsa <srivatsa@mail.uni-paderborn.de>
2020-04-06 09:48:07 +0200

    Added navigation guards to handle bad url
    Added navigation guards to handle bad url

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,19 +3,23 @@ import MoreInformation from "./components/landingPage/MoreInformation";
 import PageNotFound from "./components/PageNotFound";
 
 export const routes = [
-    { path: "", component: LandingPage },
+    { path: "", name: 'LandingPage', component: LandingPage },
     { path: "/home", component: LandingPage },
-    {   name: "moreInformation",
+    {
+        name: "moreInformation",
         path: "/MoreInformation",
         component: MoreInformation,
         props: true,
         beforeEnter: (to, from, next) => {
-            if(to.query.id !== "") {
+            if (to.query.id !== "") {
                 next();
-            } else {
-                next({name: "LandingPage"});
             }
+            else {
+                next({name:'LandingPage'});
+            }
+            
         }
     },
+
     { path: "*", component: PageNotFound },
 ];
