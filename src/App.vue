0fe093757815b748dae78f91d2977bf3c4f76838
Ben Hermann <ben.hermann@googlemail.com>
2021-09-30 12:33:11 +0200

    Typescript, Logos, etc.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen flex">
+    <img src="./assets/logo.svg" alt="DelphiHub Logo" class="mx-auto my-auto w-1/2" />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+@Options({
+  components: {},
+})
+export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
