82d602a7081d0402d293e74735ae722f11c4666d
Ben Hermann <ben.hermann@googlemail.com>
2021-12-28 14:46:56 +0100

    Removed unused code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="h-screen mx-5">
     <img src="./assets/logo.svg" alt="DelphiHub Logo" class="w-52" />
-    <!--<img src="./assets/logo.svg" alt="DelphiHub Logo" class="mx-auto my-auto w-1/2" />-->
     <router-view/>
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,8 +6,5 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
     name: "Home",
-    setup() {
-        
-    },
 })
 </script>
